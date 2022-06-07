fcaf1ecd12ea16811b2b727f3dc3bc8c237f2dea
rakeshramesan <rakeshramesan07@gmail.com>
2022-06-07 13:26:33 -0500

    initial commit

---
+++ src/main/java/com/ff/dockertest/DockertestApplication.java
@@ -0,0 +1,13 @@
+package com.ff.dockertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockertestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockertestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ff/dockertest/DockertestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ff.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
